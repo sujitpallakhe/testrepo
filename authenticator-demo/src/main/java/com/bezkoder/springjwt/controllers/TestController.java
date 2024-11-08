3f4f32aa06b7a54526d08f5519d00fc0571396e0
Sujit Pallakhe <sujit.pallakhe1997@gmail.com>
2024-11-08 18:01:08 +0530

    commited by sujit

--- authenticator-demo/src/main/java/com/bezkoder/springjwt/controllers/TestController.java
+++ authenticator-demo/src/main/java/com/bezkoder/springjwt/controllers/TestController.java
@@ -24,7 +24,7 @@ public class TestController {
   @GetMapping("/mod")
   @PreAuthorize("hasRole('MODERATOR')")
   public String moderatorAccess() {
-    return "Moderator Board -updated by Brahmi";
+    return "Moderator Board -updated by sujit";
   }
 
   @GetMapping("/admin")
